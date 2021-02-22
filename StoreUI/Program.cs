3dd971ebb38f5e609261624d1135f7a7ee0b0e83
Hans <hmittig21@gmail.com>
2021-02-21 19:24:43 -0500

    Added initial layout for project

---
+++ StoreUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace StoreUI
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Error("Item successfully purchased");  
+            Console.Read();
+        }
+    }
+}
